b12bd388e012402223968f0a1023fb11485228ea
Zorawar Brar <jorabrar2007@gmail.com>
2021-07-27 16:08:31 -0400

    added status

--- First Bot/bot.py
+++ First Bot/bot.py
@@ -1,11 +1,24 @@
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 import random as r
 import os
+from itertools import cycle
 
 intents = discord.Intents.all()
 client = commands.Bot(command_prefix = '.', intents = intents)
+status = cycle(['Minecraft', 'Mincraft'])
 
+# Startup
+@client.event
+async def on_ready():
+    change_status.start()
+    print('Bot is online.\n')
+
+@tasks.loop(seconds = 60)
+async def change_status():
+    await client.change_presence(activity = discord.Game(next(status)))
+
+# Cogs
 @client.command(help = '- Loads cogs.') 
 async def load(ctx, extension): # extension is the cog we want to load.
     try:
@@ -25,9 +38,8 @@ async def reload(ctx, extension):
     client.load_extension(f'cogs.{extension}')
     await ctx.send(f'Reloaded {extension}')
     
-# Load cogs
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
 
-client.run('token')
+client.run('ODY3NDQyMTUyNDYwMjU1MjQz.YPhKdQ.RkdBftllj1HOwMIVsB5ZIPrk1is')
